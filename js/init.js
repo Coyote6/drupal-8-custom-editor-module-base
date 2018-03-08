53f6f111ea2e476f691ade556d3f7e41ee49cce4
Tanner Naeher <tan@Tanners-MacBook.local>
2018-03-08 11:25:33 -0600

    Wrapped js to avoid conflicts.

--- js/init.js
+++ js/init.js
@@ -1,8 +1,8 @@
-// This is probably supposed to be wrapped to prevent conflict... will update soon.
-var myeditor = {
-	attach: function attach(element, format) {},
-	detach : function detach(element, format, trigger){},
-	onChange: function onChange(element, callback) {}
-};
-Drupal.editors.myeditor = myeditor;
-
+(function (Drupal) {
+	var myeditor = {
+		attach: function attach(element, format) {},
+		detach : function detach(element, format, trigger){},
+		onChange: function onChange(element, callback) {}
+	};
+	Drupal.editors.myeditor = myeditor;
+})(Drupal);
